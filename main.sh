9743b97bfe6881ed6b42bf296fbbc9cf19ca358c
gsventerprise <gsventerprise2018@gmail.com>
2021-12-12 20:51:03 -0300

    GCloud SDK

--- main.sh
+++ main.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 # Declaramos las variables 
-. ./vars/vars
+#. ./vars/vars
 #
 # Incluimos las librerias.
-. ./func/gcpCreateProject
-. ./func/gcpCreateSAkey
-. ./func/gcpDeleteDefaultSubnet
-. ./func/gcpEnableGcpApis
-. ./func/gcpEnableRoles
-. ./func/exportTFvars
+#. ./func/gcpCreateProject
+#. ./func/gcpCreateSAkey
+#. ./func/gcpDeleteDefaultSubnet
+#. ./func/gcpEnableGcpApis
+#. ./func/gcpEnableRoles
+#. ./func/exportTFvars
 . ./func/gcpAccountConfig
 
 # Verificamos si esta instalado Google Cloud SDK
 gcp_gcloudSDK
 
     ## Creamos el proyecto
-    gcp_CreateProject $PROJECT_ID $ACCOUNT_ID $SERVICE_ACCOUNT_ID $DISPLAY_NAME
+    #gcp_CreateProject $PROJECT_ID $ACCOUNT_ID $SERVICE_ACCOUNT_ID $DISPLAY_NAME
 
     ## Asignamos Role al service account:
-    gcp_EnableRoles $SERVICE_ACCOUNT_ID $PROJECT_ID
+    #gcp_EnableRoles $SERVICE_ACCOUNT_ID $PROJECT_ID
 
     ## Creamos la key de la service account
-    gpc_CreateSAkey $KEY_FILE $SERVICE_ACCOUNT_ID $PROJECT_ID
+    #gpc_CreateSAkey $KEY_FILE $SERVICE_ACCOUNT_ID $PROJECT_ID
 
     ## Habilitamos las APIs
-    gcp_EnableApis $PROJECT_ID
+    #gcp_EnableApis $PROJECT_ID
 
     ## Eliminamos la default network  
-    gpc_DeleteDefaulNetwork $PROJECT_ID
+    #gpc_DeleteDefaulNetwork $PROJECT_ID
 
     ## Exportamos la variables de entorno para Terraform y Ansible.
-    exportTFvars $PROJECT_ID $KEY_FILE
+    #exportTFvars $PROJECT_ID $KEY_FILE
